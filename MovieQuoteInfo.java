e5add3f1a91254fe853a16adbef919ca7c28e36c
Ros_Nikki <roshinikitha@gmail.com>
2025-01-28 18:39:58 -0600

    yoyoyo

---
+++ MovieQuoteInfo.java
@@ -0,0 +1,29 @@
+/* Program for Question 7, page 46: 
+* Write, compile, and test a class (MovieQuoteInfo) that displays: 
+    - Favorite movie quote 
+    - Movie it comes from 
+    - charecter who said it 
+    - Year of the movie
+    + be goofy while you still understand what is going on
+ */
+
+public class MovieQuoteInfo {       //Named class 'MovieQuoteInfo'
+    public static void main(String[] args){
+
+        System.out.println(" ");    //a blank Space for ✨aesthetics✨, next thing printed is on next line
+        System.out.println("\"you stay in Wonderland and I show you how deep the rabbit hole goes.\"");
+            //Prints the Quote, 
+            // the '\' makes sure the Double quotes are printed and dosn't cook my code. 
+    
+        System.out.println("     - Morpheus");
+             //On the next line, prints name of charecter who said quote (total legend, also my dad's obsession)
+
+        System.out.print("     The Matrix, ");      //on next line, prints out movie name      
+        System.out.println("1999");     //on same line as movie name, prints out the year movie was made
+            
+        System.out.print(" "); //on next line, a blank for ✨aesthetics✨
+       // you will see a '%' at the end, that’s 'zsh' being extra. It’s a terminal thing, not me.
+
+    }
+
+}
