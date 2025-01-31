04da7fae802a970f6bad06fdfd26053c65e85b28
Ros_Nikki <roshinikitha@gmail.com>
2025-01-31 14:39:23 -0600

    joj

--- F_to_C.java
+++ F_to_C.java
@@ -6,13 +6,13 @@ public class F_to_C {
     
     public static void main(String[] args){
 //reading and assigning f
-        Scanner reader = new Scanner(System.in); //intilizing reader
+        Scanner reader1 = new Scanner(System.in); //intilizing reader
         System.out.println("enter a temp in f"); //prompts user
-        double fer_temp = reader.nextDouble(); // reads next line 
+        double fer_temp = reader1.nextFloat(); // reads next line 
 
 //calculations (32°F − 32) × 5/9 = 0°C
         double cel_temp = (fer_temp - 32)* (5/9); 
 //prints out the result in cel
-        System.out.println( cel_temp );
+        System.out.println( cel_temp);
     }
 }
---
+++ If2021.java
@@ -0,0 +1,36 @@
+/* Program: Page 227, Question 4
+Write a program for cell phone service
+    Prompt for: 
+        + maximum monthly values for talk minutes used
+        + text messages sent
+        + gigabytes of data used
+        + then recommend the best plan for the 
+        customer’s needs
+
+    Logic: 
+        + Plan A ($49/mo): >500 minutes of talk, no data 
+        + Plan B ($55/mo): >500 minutes of talk, any text 
+        + Plan C ($61/mo): <= 500 minutes of talk, no data
+            up to 100 text msg
+        + Plan D ($70/mo): <= 500 minutes of talk, no data
+            100 text messeges or more     
+        + Plan E ($79/mo): any data 
+            up to 3 GB
+        + Plan F ($87/mo): any data 
+            3 GB or more 
+*/
+import java.util.Scanner ;
+
+public class If2021 {
+    public static void main(String[] args){
+//reading, prompting, recording 
+        Scanner reader = new Scanner(System.in); // Setting up the reader  
+        System.out.println("Enter "); // prompting the user 
+        
+
+
+
+
+    }
+    
+}
