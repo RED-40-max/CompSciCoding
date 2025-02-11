a7cfa1bca0c2fd40f1f1b56125e4b253ee6a6426
Ros_Nikki <roshinikitha@gmail.com>
2025-02-11 12:51:22 -0600

    sd

--- decision/AvrageIF.java
+++ decision/AvrageIF.java
@@ -1,102 +1,100 @@
-package decision;
 
+// /* program: question 2, If statements assignment 
+//     + prompt user to enter 3 numbers 
+//     + print out numbers in order of smallest to largest 
+//     + Average the numbers 
+// Test with: 
+//     + any 3 numbers
+//     + 2 numbers that are the same
+//     + 3 numbers that are the same
+//  */
 
-/* program: question 2, If statements assignment 
-    + prompt user to enter 3 numbers 
-    + print out numbers in order of smallest to largest 
-    + Average the numbers 
-Test with: 
-    + any 3 numbers
-    + 2 numbers that are the same
-    + 3 numbers that are the same
- */
+// import java.util.Scanner;
 
-import java.util.Scanner;
+// public class AvrageIF {
+//     public static void main(String[] args){
+// //Reading and storing values         
+//     //First Value reading, prompting, recording 
+//         Scanner FirstReader = new Scanner(System.in); //Reader initlized
+//         System.out.println("enter the first value: "); //prompt user
+//         float FirstValue = FirstReader.nextFloat(); //store the first value from the input of next line 
 
-public class AvrageIF {
-    public static void main(String[] args){
-//Reading and storing values         
-    //First Value reading, prompting, recording 
-        Scanner FirstReader = new Scanner(System.in); //Reader initlized
-        System.out.println("enter the first value: "); //prompt user
-        float FirstValue = FirstReader.nextFloat(); //store the first value from the input of next line 
+//     //Seond Value reading, prompting, recording 
+//         Scanner SecondReader = new Scanner(System.in);
+//         System.out.println("enter the second value: ");
+//         float SecondValue = SecondReader.nextFloat();    
 
-    //Seond Value reading, prompting, recording 
-        Scanner SecondReader = new Scanner(System.in);
-        System.out.println("enter the second value: ");
-        float SecondValue = SecondReader.nextFloat();    
+//     //Third Value reading, prompting, recording 
+//         Scanner ThirdReader = new Scanner(System.in);
+//         System.out.println("enter the third value: ");
+//         float ThirdValue = ThirdReader.nextFloat();   
 
-    //Third Value reading, prompting, recording 
-        Scanner ThirdReader = new Scanner(System.in);
-        System.out.println("enter the third value: ");
-        float ThirdValue = ThirdReader.nextFloat();   
+// //doing the math 
+//         float AverageVal = (FirstValue + SecondValue + ThirdValue) / 3; // finding the average of 3 numbers 
 
-//doing the math 
-        float AverageVal = (FirstValue + SecondValue + ThirdValue) / 3; // finding the average of 3 numbers 
+// /* If cascade - 9 FREAKING PERMUTATIONS. there is probably a more efficent way to do this, but 
+// this is an practice in IF statements so whatever 
+// A) 1 > 2 > 3 
+// B) 1 > 3 > 2
+// C) 2 > 1 > 3 
+// D) 2 > 3 > 1 
+// E) 3 > 2 > 1
+// F) 3 > 1 > 2
 
-/* If cascade - 9 FREAKING PERMUTATIONS. there is probably a more efficent way to do this, but 
-this is an practice in IF statements so whatever 
-A) 1 > 2 > 3 
-B) 1 > 3 > 2
-C) 2 > 1 > 3 
-D) 2 > 3 > 1 
-E) 3 > 2 > 1
-F) 3 > 1 > 2
+// ++ add >= instead of > for handeling things that are equal number 
+// */
 
-++ add >= instead of > for handeling things that are equal number 
-*/
-
-//initalize largest and smallest variables so that it dosn't error out in the system print later
-        float LargestNum = 1; 
-        float SmallestNum = 0; 
+// //initalize largest and smallest variables so that it dosn't error out in the system print later
+//         float LargestNum = 1; 
+//         float SmallestNum = 0; 
        
-// 1 is biggest 
-        if ((FirstValue >= SecondValue) && (SecondValue >= ThirdValue)) { // if A) 1 > 2 > 3 
-            LargestNum = FirstValue; // 1 is biggest
-            SmallestNum = ThirdValue; // 3 is smallest
+// // 1 is biggest 
+//         if ((FirstValue >= SecondValue) && (SecondValue >= ThirdValue)) { // if A) 1 > 2 > 3 
+//             LargestNum = FirstValue; // 1 is biggest
+//             SmallestNum = ThirdValue; // 3 is smallest
 
-        } else if ((FirstValue >= ThirdValue) && (ThirdValue >= SecondValue)) { // if B) 1 > 3 > 2
-            LargestNum = FirstValue; // 1 is biggest
-            SmallestNum = SecondValue; // 2 is smallest 
+//         } else if ((FirstValue >= ThirdValue) && (ThirdValue >= SecondValue)) { // if B) 1 > 3 > 2
+//             LargestNum = FirstValue; // 1 is biggest
+//             SmallestNum = SecondValue; // 2 is smallest 
 
-// 2 is biggest 
-        } else if ((SecondValue >= FirstValue) && (FirstValue >= ThirdValue)) { // if C) 2 > 1 > 3 
-            LargestNum = SecondValue; // 2 is bigest 
-            SmallestNum = ThirdValue;// 3 is smallest
+// // 2 is biggest 
+//         } else if ((SecondValue >= FirstValue) && (FirstValue >= ThirdValue)) { // if C) 2 > 1 > 3 
+//             LargestNum = SecondValue; // 2 is bigest 
+//             SmallestNum = ThirdValue;// 3 is smallest
 
-        } else if ((SecondValue >= ThirdValue) && (ThirdValue >= FirstValue)) { // if D) 2 > 3 > 1 
-            LargestNum = SecondValue; // 2 is bigest
-            SmallestNum = FirstValue; // 1 ist smallest
+//         } else if ((SecondValue >= ThirdValue) && (ThirdValue >= FirstValue)) { // if D) 2 > 3 > 1 
+//             LargestNum = SecondValue; // 2 is bigest
+//             SmallestNum = FirstValue; // 1 ist smallest
 
-//3 is biggest 
-        } else if ((ThirdValue >= FirstValue) && (FirstValue >= SecondValue)) { // if E) 3 > 2 > 1
-            LargestNum = ThirdValue; // 3 is biggest
-            SmallestNum = SecondValue; // 2 is smallest 
+// //3 is biggest 
+//         } else if ((ThirdValue >= FirstValue) && (FirstValue >= SecondValue)) { // if E) 3 > 2 > 1
+//             LargestNum = ThirdValue; // 3 is biggest
+//             SmallestNum = SecondValue; // 2 is smallest 
 
-        } else { // if F) 3 > 1 > 2 
-            LargestNum = ThirdValue; // 3 is biggest
-            SmallestNum = FirstValue; // 2 is smallest 
-        }
+//         } else { // if F) 3 > 1 > 2 
+//             LargestNum = ThirdValue; // 3 is biggest
+//             SmallestNum = FirstValue; // 2 is smallest 
+//         }
 
-//Printing it out 
+// //Printing it out 
 
-     System.out.println("Largest Number: " + LargestNum); // Prints Largest Number 
-     System.out.println("Smallest Number: " + SmallestNum); //Prints smallest Number 
-     System.out.println("Average of numbers: " + AverageVal);
+//      System.out.println("Largest Number: " + LargestNum); // Prints Largest Number 
+//      System.out.println("Smallest Number: " + SmallestNum); //Prints smallest Number 
+//      System.out.println("Average of numbers: " + AverageVal);
 
-    }
-}
+//     }
+// }
 
-//The alternative ways to sort it out
+// //The alternative ways to sort it out
 
-/* 
-//    + using '.Math' a 'max()' and 'min()' only returns out of two numbers 
-float largest = Math.max(FirstValue, Math.max(SecondValue, ThirdValue)); // would sort like this 1 ?> (2 >? 3)
-float smallest = Math.min(FirstValue, Math.max(SecondValue, ThirdValue); // same here but assigns a min instead 1 ?> (2 >? 3)
-*/
+// /* 
+// //    + using '.Math' a 'max()' and 'min()' only returns out of two numbers 
+// float largest = Math.max(FirstValue, Math.max(SecondValue, ThirdValue)); // would sort like this 1 ?> (2 >? 3)
+// float smallest = Math.min(FirstValue, Math.max(SecondValue, ThirdValue); // same here but assigns a min instead 1 ?> (2 >? 3)
+// */
  
-/* 
-//   +storing it into a list / array then sorting it and returning 1st and last value 
-    [FIND OUT LATER HOW TO DO THIS, IM LAZY, but it seems like a good way]
+// /* 
+// //   +storing it into a list / array then sorting it and returning 1st and last value 
+//     [FIND OUT LATER HOW TO DO THIS, IM LAZY, but it seems like a good way]
 
-*/
+// */
--- decision/CellPhone.java
+++ decision/CellPhone.java
@@ -1,81 +1,81 @@
 
-/* Program: Page 227, Question 4
+// /* Program: Page 227, Question 4
 
-Write a program for cell phone service
-    Prompt for: 
-        + maximum monthly values for talk minutes used
-        + text messages sent
-        + gigabytes of data used
-        + then recommend the best plan for the 
-        customer’s needs
+// Write a program for cell phone service
+//     Prompt for: 
+//         + maximum monthly values for talk minutes used
+//         + text messages sent
+//         + gigabytes of data used
+//         + then recommend the best plan for the 
+//         customer’s needs
 
-    Logic: 
-        + Plan A ($49/mo): <500 minutes of talk, no data, no text  gyatt sigma
-        + Plan B ($55/mo): <500 minutes of talk, no any text 
-        + Plan C ($61/mo): >= 500 minutes of talk, no data
-            up to 100 text msg
-        + Plan D ($70/mo): >= 500 minutes of talk, no data
-            100 text messeges or more     
-        + Plan E ($79/mo): any data 
-            up to 3 GB
-        + Plan F ($87/mo): any data 
-            3 GB or more (<=)
-*/
-import java.util.Scanner ;
+//     Logic: 
+//         + Plan A ($49/mo): <500 minutes of talk, no data, no text  gyatt sigma
+//         + Plan B ($55/mo): <500 minutes of talk, no any text 
+//         + Plan C ($61/mo): >= 500 minutes of talk, no data
+//             up to 100 text msg
+//         + Plan D ($70/mo): >= 500 minutes of talk, no data
+//             100 text messeges or more     
+//         + Plan E ($79/mo): any data 
+//             up to 3 GB
+//         + Plan F ($87/mo): any data 
+//             3 GB or more (<=)
+// */
+// import java.util.Scanner ;
 
-public class CellPhone {
-    public static void main(String[] args){
-//Storing values 
-    //Talk minutes used reading, prompting, recording 
-        Scanner TalkMinReader = new Scanner(System.in); // Setting up the reader  
-        System.out.println("Enter Monthly Talk Minutes: "); // prompting the user 
-        int TalkMinValue = TalkMinReader.nextInt(); // read next int and record the MSG
+// public class CellPhone {
+//     public static void main(String[] args){
+// //Storing values 
+//     //Talk minutes used reading, prompting, recording 
+//         Scanner TalkMinReader = new Scanner(System.in); // Setting up the reader  
+//         System.out.println("Enter Monthly Talk Minutes: "); // prompting the user 
+//         int TalkMinValue = TalkMinReader.nextInt(); // read next int and record the MSG
 
-    //Text messages used reading, prompting, recording 
-        Scanner TextReader = new Scanner(System.in); // Setting up the reader 
-        System.out.println("Enter Monthly Text Messege: "); // prompting the user 
-        int TextMsgValue = TextReader.nextInt(); // read next int and record the MSG
+//     //Text messages used reading, prompting, recording 
+//         Scanner TextReader = new Scanner(System.in); // Setting up the reader 
+//         System.out.println("Enter Monthly Text Messege: "); // prompting the user 
+//         int TextMsgValue = TextReader.nextInt(); // read next int and record the MSG
     
-    //Gigabytes of data used reading, prompt, recording 
-        Scanner GB_dataReader = new Scanner(System.in); // Setting up the reader  
-        System.out.println("Enter Monthly GB of data used: "); // prompting the user 
-        double GB_data = GB_dataReader.nextDouble();
-//inital base print response 
-        System.out.println(" Thank you for Selecting Horizon Phones. \nyour ideal Plan based on you're monthly plan is:");
+//     //Gigabytes of data used reading, prompt, recording 
+//         Scanner GB_dataReader = new Scanner(System.in); // Setting up the reader  
+//         System.out.println("Enter Monthly GB of data used: "); // prompting the user 
+//         double GB_data = GB_dataReader.nextDouble();
+// //inital base print response 
+//         System.out.println(" Thank you for Selecting Horizon Phones. \nyour ideal Plan based on you're monthly plan is:");
 
-// If cascade
-    //500 min or less AND no data 
-    if ((TalkMinValue < 500) && (GB_data == 0)) { 
-        if (TextMsgValue == 0) { //if no messeges = plan A
-            System.out.print("Plan A ($49/mo)");
-        } 
-        else { //otherwise plan B 
-            System.out.print("Plan B ($55/mo)");
-        }
-    }
+// // If cascade
+//     //500 min or less AND no data 
+//     if ((TalkMinValue < 500) && (GB_data == 0)) { 
+//         if (TextMsgValue == 0) { //if no messeges = plan A
+//             System.out.print("Plan A ($49/mo)");
+//         } 
+//         else { //otherwise plan B 
+//             System.out.print("Plan B ($55/mo)");
+//         }
+//     }
 
-    // 500 or more minutes AND No data
-    else if ((TalkMinValue >= 500) && (GB_data == 0)) { 
-        if (TextMsgValue < 100) { //if less then 100 msg then plan c 
-            System.out.print("Plan C ($61/mo)");
-        } else { //otherwise plan B
-            System.out.print("Plan D ($70/mo)");
-        }
-    } 
+//     // 500 or more minutes AND No data
+//     else if ((TalkMinValue >= 500) && (GB_data == 0)) { 
+//         if (TextMsgValue < 100) { //if less then 100 msg then plan c 
+//             System.out.print("Plan C ($61/mo)");
+//         } else { //otherwise plan B
+//             System.out.print("Plan D ($70/mo)");
+//         }
+//     } 
 
-    // Any data usage at all 
-    else if (GB_data > 0) { 
-        if (GB_data < 3) { //less then 3GB is plan E 
-            System.out.print("Plan E ($79/mo)");
-        } 
-        else { // otherwise is equal to 3GB
-            System.out.print("Plan F ($87/mo)");
-            }
-        }
+//     // Any data usage at all 
+//     else if (GB_data > 0) { 
+//         if (GB_data < 3) { //less then 3GB is plan E 
+//             System.out.print("Plan E ($79/mo)");
+//         } 
+//         else { // otherwise is equal to 3GB
+//             System.out.print("Plan F ($87/mo)");
+//             }
+//         }
 
-    }
+//     }
 
-}
-//ITS OVER!!! the cascade was ...
-// a process. 
-// but i got it eventually!
+// }
+// //ITS OVER!!! the cascade was ...
+// // a process. 
+// // but i got it eventually!
